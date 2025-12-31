885fd940f0e1dfbe13d4a3f3d6f7e876988c3657
Zaprit <zaprit@hugespaceship.io>
2025-12-31 01:53:57 +0000

    Clean up comments in assembly

--- hooks.s
+++ hooks.s
@@ -4,11 +4,9 @@ RNPCSRHook:
     lwz r3, 0(r3) # read pointer at r3 into r3
     lbz r3, 39(r3) # read the resource type from the structure
 
-    # Find what condition register was last used for the if statement we're in, re-use that to avoid chaos
-    # guess we using cr0, as that's what beq needs
     cmpwi cr7, r3, 0xB # 0xB is the script type
-    beq cr7, isscript
-    b notscript
+    beq cr7, isscript # Branch to isscript if type is 0xB
+    b notscript # Otherwise branch to notscript
 
 isscript:
     ba 0x81B40 # Go to the error handling for when RNP isn't working
