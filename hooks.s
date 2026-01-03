47325a258d9b9e15511798a040ff5c880b57c8bd
Zaprit <zaprit@hugespaceship.io>
2026-01-03 22:24:04 +0000

    Initial hook for lbp2 1.33

--- hooks.s
+++ hooks.s
@@ -16,3 +16,17 @@ notscript:
     clrldi r3, r25, 32 # Reset r3
     bla 0x7D1EC # AddToCSRQueue
     ba 0x81BCC # instruction after our hook branch
+
+.global LBP2ScriptHook
+LBP2ScriptHook:
+    beq cr7, dontload # relocated branch from original function, uses a label here but is mostly equivalent
+
+    # we shouldn't need to care about r0, as anything that uses it sets it immediately
+    lwz r0, 0(r31) # r31 has the pointer to the CSR
+    lbz r0, 36(r0) # 36 bytes in is the resource type
+    cmpwi cr7, r0, 0x1 # if the resource is a script
+    beq cr7, dontload # don't load it
+    ba 0x143D78 # continue to original function
+
+dontload:
+    ba 0x153D3C # Return 0 in resource check function
