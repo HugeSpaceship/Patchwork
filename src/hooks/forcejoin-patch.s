11f8a8dba9cab992ec7f904b6e5c1e4981e073bf
I-Knight-I <kpsc1822@gmail.com>
2026-07-14 19:56:57 +0100

    Fix autojoin friends without invite on LBP2

--- src/hooks/forcejoin-patch.s
+++ src/hooks/forcejoin-patch.s
@@ -43,7 +43,7 @@ LBP2ForceJoinPatch:
     bla 0x264148                        # Call 'CNetworkFriendsManager::FindFriendDataEntry(FriendManager *this, NetworkClientId friend, null)'
 
     cmpwi cr7, r3, 0x0
-    beq cr7, 0x0c                       # Null-check [0x0285ff4]
+    beq cr7, 0x10                       # Null-check [0x0285ff4]
     lwz r6, 0xe8(r3)                    # Load CFriendData->WeInvitedThisUserToJoinUs float into r6
     cmpwi cr7, r6, 0x0
     bgt cr7, 0x43                       # If WeInvitedThisUserToJoinUs > 0.0, goto success path (auto-accept) [0x0286030]
@@ -54,19 +54,19 @@ LBP2ForceJoinPatch:
     rldicl r3, r25, 0x0, 0x20           # r3 = CGameHostParties *this
     rldicl r4, r28, 0x0, 0x20           # r4 = 'message' parameter
     or r5, r27, r27                     # r5 = 'source' parameter
-    li r7, 0x0                          # r6/r7 = NULL (r6 is already 0x0 here as a result of the cmpwi check above, so we save 4 bytes)
+    li r6, 0x0                          # r6 = false (isLocalPlayerJoining)
+    li r7, 0x0                          # r7 = NULL
     li r8, 0xc
     bla 0x285914                        # Call 'CGameHostParties::DealWithRequestJoinParty'
     
     rlwinm r3, r3, 0x0, 0x18, 0x1f
     cmpwi cr7, r3, 0x0
-    bne cr7, -0xec                      # DealWithRequestJoinParty success/fail check [0x0285f28]
+    bne cr7, -0xf0                      # DealWithRequestJoinParty success/fail check [0x0285f28]
     rldicl r3, r25, 0x0, 0x20
     rldicl r4, r28, 0x0, 0x20
     or r5, r27, r27
     bla 0x282440                        # Call 'CGameHostParties::ForwardRequestJoinPartyToLocalClient'
-    b -0x100                            # [0x0285f28]
-    ori r0, r0, 0x0                     # !Required! NOP
+    b -0x104                            # [0x0285f28]
 # End address : 0x028602c
 
 
